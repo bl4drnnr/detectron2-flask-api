fb56e746ca76113d714b681b80016146a1d4997c
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-12-21 08:02:14 +0100

    input output images names

--- src/api_responses.py
+++ src/api_responses.py
@@ -1,10 +1,16 @@
+from flask import jsonify
+
+
 class ApiResponse(Exception):
     def __init__(self, response, status_code=200):
         self.response = {"data": response, "status_code": status_code}
         self.status_code = status_code
+    
+    def get_response(self):
+        return jsonify(self.response)
 
 
-class WrongPayload(Exception):
+class WrongPayload(ApiResponse):
     def __init__(self, response='Wrong payload', status_code=500):
         self.response = {"data": response, "status_code": status_code}
         self.status_code = status_code

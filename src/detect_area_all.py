2ee654a9f54a59716f56873c8e644167090c2b32
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-12-22 18:01:22 +0100

    partial images names

--- src/detect_area_all.py
+++ src/detect_area_all.py
@@ -32,17 +32,18 @@ class DetectAreaAll(Resource):
                 detected_rois = detect_area(input_image_name, output_image_name, threshold_value)
 
                 out_image_base = convert_image_to_base64(output_image_name)
-                save_encoded_string(f'./output/string_images/OUTPUT_JSON_BASE.txt', out_image_base)
+                save_encoded_string(f'./output/string_images/OUTPUT_JSON_BASE_{file_name.split(".")[0]}.txt', out_image_base)
                 out_image_string = out_image_base.decode('UTF-8')
 
                 out_partial_image_table = []
                 for i in range(len(detected_rois)):
                     out_part_image_base = convert_image_to_base64(
-                        f'./output/images/OUTPUT_PARTIAL_IMAGE_{i}.jpg'
+                        f'./output/images/OUTPUT_PARTIAL_IMAGE_{file_name.split(".")[0]}_{i}.jpg'
                     )
                     
                     save_encoded_string(
-                        f'./output/string_images/OUTPUT_PARTIAL_IMAGE_JSON_BASE_{i}.txt', out_part_image_base
+                        f'./output/string_images/OUTPUT_PARTIAL_IMAGE_JSON_BASE_{file_name.split(".")[0]}_{i}.txt', 
+                        out_part_image_base
                     )
 
                     out_partial_image_string = out_part_image_base.decode('UTF-8')

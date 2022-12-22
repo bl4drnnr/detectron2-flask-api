6e96cd330c6449c475f896582507442df7e6bc90
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-12-22 17:50:02 +0100

    detecting all areas

--- src/detect_area_all.py
+++ src/detect_area_all.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_restful import Api, Resource
 
 from src.helpers.image_converter import *
@@ -13,20 +14,46 @@ REQUIRED_PAYLOAD = [
 class DetectAreaAll(Resource):
     def post(self):
         try:
-            payload = requests.get_json()
+            payload = request.get_json()
             check_required_payload(payload, REQUIRED_PAYLOAD)
 
-            files_names = os.listdir('./input/images')
             threshold_value = payload['threshold_value']
+            all_files = os.listdir('./input/images')
+            response = []
 
-            for file in files_names:
-                image_string = convert_image_to_base64(f'./input/images/{file}')
-                output_image_name = f"./output/images/{file}"
-                input_image_name = f"./input/images/{file}"
+            if len(all_files) == 0:
+                response = {"message": "no input files"}
+                return ApiResponse(response).get_response()
+
+            for file_name in all_files:
+                output_image_name = f"./output/images/{file_name}"
+                input_image_name = f"./input/images/{file_name}"
                 
                 detected_rois = detect_area(input_image_name, output_image_name, threshold_value)
 
-            response = {}
+                out_image_base = convert_image_to_base64(output_image_name)
+                save_encoded_string(f'./output/string_images/OUTPUT_JSON_BASE.txt', out_image_base)
+                out_image_string = out_image_base.decode('UTF-8')
+
+                out_partial_image_table = []
+                for i in range(len(detected_rois)):
+                    out_part_image_base = convert_image_to_base64(
+                        f'./output/images/OUTPUT_PARTIAL_IMAGE_{i}.jpg'
+                    )
+                    
+                    save_encoded_string(
+                        f'./output/string_images/OUTPUT_PARTIAL_IMAGE_JSON_BASE_{i}.txt', out_part_image_base
+                    )
+
+                    out_partial_image_string = out_part_image_base.decode('UTF-8')
+                    out_partial_image_table.append({f'partial_image_{i}': out_partial_image_string})
+                
+                response.append({
+                    'input_image_name': file_name,
+                    'detected_bounding_boxes': detected_rois,
+                    'partial_images': out_partial_image_table,
+                    'output_image': out_image_string
+                })
 
             return ApiResponse(response).get_response()
 
